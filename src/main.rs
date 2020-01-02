b906f8a1fbac38ce0f8565a7cfb1c44cb18862fe
Mårten Kongstad <marten.kongstad@gmail.com>
2020-01-02 15:43:03 +0100

    Optimize how to extract resources.arsc from zip
    In an apk zip file, resources.arsc is always stored (i.e. never
    compressed). This allows Android to memory map the zip file. Implement
    the same optimization.
    
    On my system, using a release build of arsc to parse framework-res.apk,
    hyperfine says the runtime is improved by a factor of 8:
    
    Before:
      Time (mean ± σ):     175.0 ms ±   3.6 ms    [User: 55.7 ms, System: 118.7 ms]
    
    After:
      Time (mean ± σ):      21.8 ms ±   3.4 ms    [User: 15.3 ms, System: 6.4 ms]
    
    Signed-off-by: Mårten Kongstad <marten.kongstad@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use arsc::{Chunk, ChunkIterator};
 use clap::{value_t, App, Arg};
+use memmap::MmapOptions;
 use std::fs::File;
-use std::io::prelude::*;
-use zip::ZipArchive;
+use zip::{CompressionMethod, ZipArchive};
 
 fn iterate(iter: ChunkIterator, depth: usize) {
     let indent = (0..depth * 4).map(|_| " ").collect::<String>();
@@ -39,17 +39,32 @@ fn iterate(iter: ChunkIterator, depth: usize) {
 }
 
 fn main() {
+    // parse command line arguments
     let opts = App::new("arsc")
         .arg(Arg::with_name("apk").takes_value(true).required(true))
         .get_matches();
+
+    // memory map APK
     let apk = value_t!(opts.value_of("apk"), String).unwrap();
     let file = File::open(apk).expect("failed to open file");
-    let mut zip = ZipArchive::new(file).expect("failed to open zip");
-    let mut entry = zip
+    let mmap = unsafe { MmapOptions::new().map(&file).unwrap() };
+
+    // read zip header, entry header
+    let reader = std::io::Cursor::new(mmap.as_ref());
+    let mut zip = ZipArchive::new(reader).expect("failed to open zip");
+    let entry = zip
         .by_name("resources.arsc")
         .expect("failed to unzip resources.arsc");
-    let mut buf = Vec::new();
-    entry.read_to_end(&mut buf).unwrap();
-    let iter = ChunkIterator::new(&buf);
+    if entry.compression() != CompressionMethod::Stored {
+        panic!("resources.arsc compressed");
+    }
+
+    // "extract" the non-compressed entry
+    let begin = entry.data_start() as usize;
+    let end = begin + entry.size() as usize;
+    let buf = &mmap[begin..end];
+
+    // traverse chunks
+    let iter = ChunkIterator::new(buf);
     iterate(iter, 0);
 }
