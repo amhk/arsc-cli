94c6cdb721f1973234f894ee90028a031709fd70
Mårten Kongstad <marten.kongstad@gmail.com>
2020-01-15 21:02:36 +0100

    Add methods to extract parts of a resource ID
    Add methods to get the package, type or entry ID of a resource ID.
    
    Signed-off-by: Mårten Kongstad <marten.kongstad@gmail.com>

--- arsc/src/resources.rs
+++ arsc/src/resources.rs
@@ -10,6 +10,18 @@ impl ResourceId {
             id: ((package_id as u32) << 24) | ((type_id as u32) << 16) | entry_id as u32,
         }
     }
+
+    pub fn package_id(&self) -> u8 {
+        ((self.id & 0xff00_0000) >> 24) as u8
+    }
+
+    pub fn type_id(&self) -> u8 {
+        ((self.id & 0x00ff_0000) >> 16) as u8
+    }
+
+    pub fn entry_id(&self) -> u16 {
+        (self.id & 0x0000_ffff) as u16
+    }
 }
 
 impl fmt::Debug for ResourceId {
@@ -26,5 +38,13 @@ mod tests {
     fn from_parts() {
         let resid = ResourceId::from_parts(0x7f, 0x02, 0x0001);
         assert_eq!(resid.id, 0x07f020001);
+    }
+
+    #[test]
+    fn parts() {
+        let resid = ResourceId::from_parts(0x7f, 0x02, 0x0001);
+        assert_eq!(resid.package_id(), 0x7f);
+        assert_eq!(resid.type_id(), 0x02);
+        assert_eq!(resid.entry_id(), 0x0001);
     }
 }
