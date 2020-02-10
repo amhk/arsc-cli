55a324f82e757ca34c81d321da6f2352a22c1596
Mårten Kongstad <marten.kongstad@gmail.com>
2020-02-10 20:08:22 +0100

    Add ResourceValue, ResourceConfiguration
    Add public structs for the value and configuration of a resource. Like
    ResourceId, these are returned to the caller of the arsc::table
    functions and are self-contained as opposed to the internal
    arsc::table::Loaded* structs.
    
    Signed-off-by: Mårten Kongstad <marten.kongstad@gmail.com>

--- arsc/src/resources.rs
+++ arsc/src/resources.rs
@@ -40,6 +40,50 @@ impl fmt::Debug for ResourceId {
     }
 }
 
+#[derive(Debug)]
+pub enum ResourceValue {
+    Null,
+    Reference(ResourceId),
+    Attribute(ResourceId),
+    String(String),
+    Float(f32),
+    Dimension(f32),
+    Fraction(f32),
+    IntDec(i32),
+    IntHex(i32),
+    Boolean(bool),
+    ColorArgb8(f32, f32, f32, f32),
+    ColorRgb8(f32, f32, f32),
+    ColorArgb4(f32, f32, f32, f32),
+    ColorRgb4(f32, f32, f32),
+    Array(Vec<(ResourceId, ResourceValue)>),
+}
+
+pub struct ResourceConfiguration {
+    #[allow(dead_code)]
+    pub imsi: u32,
+    #[allow(dead_code)]
+    pub locale: u32,
+    #[allow(dead_code)]
+    pub screen_type: u32,
+    #[allow(dead_code)]
+    pub input: u32,
+    #[allow(dead_code)]
+    pub screen_size: u32,
+    #[allow(dead_code)]
+    pub version: u32,
+    #[allow(dead_code)]
+    pub screen_config: u32,
+    #[allow(dead_code)]
+    pub screen_size_dp: u32,
+}
+
+impl fmt::Debug for ResourceConfiguration {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "ResourceConfiguration {{ TODO(#10) }}")
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::ResourceId;
